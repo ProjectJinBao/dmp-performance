77e22ae80e4c73d9a8d2cd34ec89f9ac6e4e279f
徐旻 <xumin030@huazhu.com>
2019-05-27 11:47:06 +0800

    update test

---
+++ dmp_2_0_1/router_service_token_token/dmp_test.py
@@ -0,0 +1,22 @@
+from locust import HttpLocust, TaskSet, task
+import json
+
+class WebsitTasks(TaskSet):
+
+    @task
+    def s1_200ms1k(self):
+
+        headers = {"DEMO":"TEST"}
+
+        with self.client.get("/ps/s1/200ms/1k", headers=headers, catch_response=True) as response:
+            if response.status_code == 200:
+                pass
+            else:
+                response.failure(response)
+
+class WebsiteUser(HttpLocust):
+    task_set = WebsitTasks
+    host = "http://192.168.0.83:32677"
+    # host = "http://192.168.0.105:31251"
+    min_wait = 0
+    max_wait = 0
